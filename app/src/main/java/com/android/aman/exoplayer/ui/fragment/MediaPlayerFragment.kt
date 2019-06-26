193bdcc8417a9c7f9d25a9f9ee62488ca52e32aa
amankumar367 <kumaraman367@gmail.com>
2019-06-27 00:22:48 +0530

    Handle landscape mode

--- app/src/main/java/com/android/aman/exoplayer/ui/fragment/MediaPlayerFragment.kt
+++ app/src/main/java/com/android/aman/exoplayer/ui/fragment/MediaPlayerFragment.kt
@@ -1,8 +1,9 @@
-package com.android.aman.exoplayer.ui.fragment
+package com.android.aman. exoplayer.ui.fragment
 
 import android.content.res.Configuration
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
